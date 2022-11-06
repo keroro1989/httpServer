929cfa87566070eacccc6fd3e59b3ed90afafc15
Kero <keroro1989@gmail.com>
2022-11-06 20:44:27 +0800

    res練習

--- test.js
+++ test.js
@@ -1,8 +1,9 @@
 const http = require('http').createServer((req,res)=>{
     //在這裡設定伺服器接收到客戶端請求後的動作。
-
-    console.log(req.url); //印出客戶端網址
-    console.log(req.method); //印出客戶端向伺服器發出請求的方法
+    
+    //伺服器回應的內容
+    res.write('Hi!'); 
+    res.end(); 
 
 }); 
 
